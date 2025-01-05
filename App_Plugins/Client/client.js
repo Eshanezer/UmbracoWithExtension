0dfe6c9ed12050bdd9827a1796d6e75c2966d681
Eshan Dilmina <dilmina.eshan.d@outlook.com>
2025-01-05 16:02:37 +0200

    Auth user and system users listing added.

--- App_Plugins/Client/client.js
+++ App_Plugins/Client/client.js
@@ -1,47 +1,93 @@
-import { LitElement as c, html as i, css as n, customElement as b } from "@umbraco-cms/backoffice/external/lit";
-import { UmbElementMixin as d } from "@umbraco-cms/backoffice/element-api";
-var h = Object.defineProperty, p = Object.getOwnPropertyDescriptor, y = (s, o, m, l) => {
-  for (var e = l > 1 ? void 0 : l ? p(o, m) : o, t = s.length - 1, r; t >= 0; t--)
-    (r = s[t]) && (e = (l ? r(o, m, e) : r(e)) || e);
-  return l && e && h(o, m, e), e;
-};
-let a = class extends d(c) {
+import { UMB_CURRENT_USER_CONTEXT as m } from "@umbraco-cms/backoffice/current-user";
+import { LitElement as u, html as c, repeat as _, css as v, state as p, customElement as y } from "@umbraco-cms/backoffice/external/lit";
+import { UmbElementMixin as f } from "@umbraco-cms/backoffice/element-api";
+import { UmbUserCollectionRepository as b } from "@umbraco-cms/backoffice/user";
+var U = Object.defineProperty, w = Object.getOwnPropertyDescriptor, h = (e) => {
+  throw TypeError(e);
+}, d = (e, r, t, o) => {
+  for (var s = o > 1 ? void 0 : o ? w(r, t) : r, i = e.length - 1, n; i >= 0; i--)
+    (n = e[i]) && (s = (o ? n(r, t, s) : n(s)) || s);
+  return o && s && U(r, t, s), s;
+}, g = (e, r, t) => r.has(e) || h("Cannot " + t), D = (e, r, t) => (g(e, r, "read from private field"), t ? t.call(e) : r.get(e)), C = (e, r, t) => r.has(e) ? h("Cannot add the same private member more than once") : r instanceof WeakSet ? r.add(e) : r.set(e, t), l;
+let a = class extends f(u) {
+  constructor() {
+    super(), this._userData = [], C(this, l, new b(this)), this.consumeContext(m, (e) => {
+      this._observeCurrentUser(e);
+    }), this._getPagedUserData();
+  }
+  async _observeCurrentUser(e) {
+    this.observe(e.currentUser, (r) => {
+      this._currentUser = r;
+    });
+  }
+  async _getPagedUserData() {
+    const { data: e } = await D(this, l).requestCollection();
+    this._userData = (e == null ? void 0 : e.items) ?? [];
+  }
   render() {
-    return i`
-      <h1>
-        <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
-        Dashboard
-      </h1>
-      <div>
-        <p>
-          <umb-localize key="welcomeDashboard_bodytext">
-            This is the Backoffice. From here, you can modify the content,
-            media, and settings of your website.
-          </umb-localize>
-        </p>
-        <p>
-          <umb-localize key="welcomeDashboard_copyright">
-            © Sample Company 20XX
-          </umb-localize>
-        </p>
-      </div>
+    var e;
+    return c`
+    	    <h1>
+      	        <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
+       		${((e = this._currentUser) == null ? void 0 : e.name) ?? "Unknown"}!
+     	    </h1>
+    	    <div>
+      		<p>
+       		    <umb-localize key="welcomeDashboard_bodytext">
+         		This is the Backoffice. From here, you can modify the content,
+         		media, and settings of your website.
+       		    </umb-localize>
+      		</p>
+      		<p>
+        	    <umb-localize key="welcomeDashboard_copyright">
+          		© Sample Company 20XX
+        	    </umb-localize>
+      		</p>
+    	    </div>
+  	<div id="users-wrapper">
+        ${_(this._userData, (r) => r.unique, (r) => this._renderUser(r))}
+    </div>
     `;
   }
+  _renderUser(e) {
+    return c`<div class="user">
+		<div>${e.name}</div>
+		<div>${e.email}</div>
+		<div>${e.state}</div>
+	</div>`;
+  }
 };
+l = /* @__PURE__ */ new WeakMap();
 a.styles = [
-  n`
-      :host {
-        display: block;
-        padding: 24px;
-      }
-    `
+  v`
+		:host {
+			display: block;
+			padding: 24px;
+		}
+
+		#users-wrapper {
+			border: 1px solid lightgray;
+		}
+		.user {
+			padding: 5px 10px;
+		}
+		.user:not(:first-child) {
+			border-top: 1px solid lightgray;
+		}
+	`
 ];
-a = y([
-  b("my-welcome-dashboard")
+d([
+  p()
+], a.prototype, "_currentUser", 2);
+d([
+  p()
+], a.prototype, "_userData", 2);
+a = d([
+  y("my-welcome-dashboard")
 ], a);
-const _ = a;
+const $ = a;
 export {
   a as MyWelcomeDashboardElement,
-  _ as default
+  $ as default
 };
 //# sourceMappingURL=client.js.map
--- Client/src/welcome-dashboard.element.ts
+++ Client/src/welcome-dashboard.element.ts
@@ -1,45 +1,92 @@
-import { LitElement, css, html, customElement} from "@umbraco-cms/backoffice/external/lit";
+import { type UmbCurrentUserModel, UMB_CURRENT_USER_CONTEXT } from "@umbraco-cms/backoffice/current-user";
+import { LitElement, css, html, customElement, state, repeat  } from "@umbraco-cms/backoffice/external/lit";
 import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
+import { type UmbUserDetailModel, UmbUserCollectionRepository } from '@umbraco-cms/backoffice/user';
 
 @customElement('my-welcome-dashboard')
 export class MyWelcomeDashboardElement extends UmbElementMixin(LitElement) {
+    @state()
+    private _currentUser?: UmbCurrentUserModel;
 
-  render() {
-    return html`
-      <h1>
-        <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
-        Dashboard
-      </h1>
-      <div>
-        <p>
-          <umb-localize key="welcomeDashboard_bodytext">
-            This is the Backoffice. From here, you can modify the content,
-            media, and settings of your website.
-          </umb-localize>
-        </p>
-        <p>
-          <umb-localize key="welcomeDashboard_copyright">
-            © Sample Company 20XX
-          </umb-localize>
-        </p>
-      </div>
+    @state()
+    private _userData: Array<UmbUserDetailModel> = [];
+
+    #userRepository = new UmbUserCollectionRepository(this);
+
+    constructor() {
+        super();
+        this.consumeContext(UMB_CURRENT_USER_CONTEXT, (instance) => {
+            this._observeCurrentUser(instance);
+        });
+        this._getPagedUserData();
+    }
+
+    private async _observeCurrentUser(instance: typeof UMB_CURRENT_USER_CONTEXT.TYPE) {
+        this.observe(instance.currentUser, (currentUser) => {
+            this._currentUser = currentUser;
+        });
+    }
+    private async _getPagedUserData() {
+        const { data } = await this.#userRepository.requestCollection();
+        this._userData = data?.items ?? [];
+    }
+
+    render() {
+        return html`
+    	    <h1>
+      	        <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
+       		${this._currentUser?.name ?? "Unknown"}!
+     	    </h1>
+    	    <div>
+      		<p>
+       		    <umb-localize key="welcomeDashboard_bodytext">
+         		This is the Backoffice. From here, you can modify the content,
+         		media, and settings of your website.
+       		    </umb-localize>
+      		</p>
+      		<p>
+        	    <umb-localize key="welcomeDashboard_copyright">
+          		© Sample Company 20XX
+        	    </umb-localize>
+      		</p>
+    	    </div>
+  	<div id="users-wrapper">
+        ${repeat(this._userData, (user) => user.unique, (user) => this._renderUser(user))}
+    </div>
     `;
-  }
+    }
+    private _renderUser(user: UmbUserDetailModel) {
+	return html`<div class="user">
+		<div>${user.name}</div>
+		<div>${user.email}</div>
+		<div>${user.state}</div>
+	</div>`;
+}
 
-  static styles = [
-    css`
-      :host {
-        display: block;
-        padding: 24px;
-      }
-    `,
-  ];
+    static styles = [
+	css`
+		:host {
+			display: block;
+			padding: 24px;
+		}
+
+		#users-wrapper {
+			border: 1px solid lightgray;
+		}
+		.user {
+			padding: 5px 10px;
+		}
+		.user:not(:first-child) {
+			border-top: 1px solid lightgray;
+		}
+	`,
+];
 }
 
 export default MyWelcomeDashboardElement;
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'my-welcome-dashboard': MyWelcomeDashboardElement;
-  }
+    interface HTMLElementTagNameMap {
+        'my-welcome-dashboard': MyWelcomeDashboardElement;
+    }
 }
